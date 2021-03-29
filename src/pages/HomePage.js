a232f3de9bfb8a2589f67b5037b80736e74b7bb1
Alex <alex37888@gmail.com>
2021-03-29 01:44:30 -0700

    Front-end:link routing,NavBar,projects list,

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HomePage = () => (
+  <>
+    <h1> Hi, Welcome to my blog!</h1>
+    <p> Feel free to checkout my journey, work, or connect!</p>
+  </>
+);
+
+export default HomePage;
