e80254505c3c719ed8a228c48d146960cf770745
Alex <alex37888@gmail.com>
2021-06-09 16:51:38 -0700

    Added name and subject line to contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -7,7 +7,9 @@ import { Button } from 'bootstrap';
 
 
 const ContactForm = () => {
+    const [name,setName] = React.useState("");
     const [email,setEmail] = React.useState("");
+    const [subject, setSubject] = React.useState("");
     const [message,setMessage] = React.useState("");
     const contactFormRef = useRef();
 
@@ -20,11 +22,29 @@ const ContactForm = () => {
             <form className="contact-body">
                 <Row>
                     <Col sm={2}>
+                        <p>Name: </p>
+                    
+                    </Col>
+                    <Col sm={6} lg={8}>
+                        <input type='text' className="inputBox" onChange={event => setName(event.target.value)}></input>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={2}>
                         <p>Email: </p>
                     
                     </Col>
                     <Col sm={6} lg={8}>
                         <input type='text' className="inputBox" onChange={event => setEmail(event.target.value)}></input>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={2}>
+                        <p>Subject:  </p>
+                    
+                    </Col>
+                    <Col sm={6} lg={8}>
+                        <input type='text' className="inputBox" onChange={event => setSubject(event.target.value)}></input>
                     </Col>
                 </Row>
                 <Row>
@@ -39,6 +59,17 @@ const ContactForm = () => {
                 </Row>
                 <Row >
                     <Col sm={6} lg={10} style={{justifyContent: 'flex-end'}}> 
+                        
+                        //TODO: Write API request to server to send email
+                        //TODO: Add name and subject lines
+                        //TODO: Add a clear button, text auto clears after send
+                        {/* use the endpoint /api/contact 
+                        Req params{
+                            name: 'string',
+                            email: 'string',
+                            subject: 'string',
+                            message: 'string',
+                        } */}
                         <button id="contactSubmitBtn">
                             Send Message
                         </button>
